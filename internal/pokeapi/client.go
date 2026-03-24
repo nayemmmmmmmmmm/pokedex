3188fdf119d873adbfeff48ba2351e8ee36f78ec
Arafat Hasan <nayemarafat786@gmail.com>
2026-03-24 15:06:32 +0600

    Added cache

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/nayemmmmmmmmmm/pokedex/internal/pokecache"
 )
 
 // Client _
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
 // NewClient _
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
